56cfb9c6490d638d7f2f060c86cb200fb4d6782e
ecaichen@gmail.com <ecaichen@gmail.com>
2015-09-06 22:32:22 -0700

    use stringutil package

--- api.go
+++ api.go
@@ -1,7 +1,18 @@
 package main
 
 import "fmt"
+import "github.com/showwayer/stringutil"
+import "os"
 
 func main() {
 	fmt.Printf("Hello, World!\n")
+	fmt.Println("Creating temp file...")
+	myFile, err := os.Create("tmp.txt")
+	if err != nil {
+		fmt.Println("Fail to create file!")
+	}
+	myFile.WriteString("sample text goes into this tmp file")
+	myFile.Close()
+	//here we call a function in stringutil
+	fmt.Printf(stringutil.Reverse("!DOOG") + "\n")
 }
