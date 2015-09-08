e0982e09e9da7bd67488368b61d4016097209e9d
ecaichen@gmail.com <ecaichen@gmail.com>
2015-09-07 17:17:37 -0700

    minor api update

--- api.go
+++ api.go
@@ -5,14 +5,15 @@ import "github.com/showwayer/stringutil"
 import "os"
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	fmt.Println("Starting API Server...")
 	fmt.Println("Creating temp file...")
 	myFile, err := os.Create("tmp.txt")
 	if err != nil {
 		fmt.Println("Fail to create file!")
+		os.Exit(-1)
 	}
 	myFile.WriteString("sample text goes into this tmp file")
-	myFile.Close()
+	err = myFile.Close()
 	//here we call a function in stringutil
-	fmt.Printf(stringutil.Reverse("!DOOG") + "\n")
+	fmt.Printf(stringutil.Reverse("!ENOD") + "\n")
 }
