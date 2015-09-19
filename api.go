295e1b9e7b283fb9e967c20ed867e472fa0db692
ecaichen@gmail.com <ecaichen@gmail.com>
2015-09-19 11:54:46 -0700

    add missing change

--- api.go
+++ api.go
@@ -9,6 +9,7 @@ import "os"
 func main() {
 	a := 1
 	b := 3
+	ow := "Google"
 	fmt.Println("Starting API Server...")
 	fmt.Println("Creating temp file...")
 	myFile, err := os.Create("tmp.txt")
@@ -19,8 +20,15 @@ func main() {
 	myFile.WriteString("sample text goes into this tmp file")
 	err = myFile.Close()
 	//here we call a function in stringutil
-	fmt.Printf(stringutil.Reverse("!ENOD") + "\n")
-	fmt.Printf("%d + %d = %d", a, b, stringutil.Add(a, b))
+	fmt.Printf("Reversing %s...%s...\n", ow, stringutil.Reverse(ow))
+	fmt.Printf("%d + %d = %d\n", a, b, stringutil.Add(a, b))
+
+	myChannel := make(chan bool, 1)
+	go worker(myChannel)
+
+	//read and block the amin thread
+	<-myChannel
+
 	fmt.Println("The end")
 }
 
