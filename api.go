36d92cbd0f5adad978f188390aed1d9647588b30
ecaichen@gmail.com <ecaichen@gmail.com>
2015-09-19 11:48:52 -0700

    adding channel example

--- api.go
+++ api.go
@@ -1,10 +1,14 @@
 package main
 
 import "fmt"
+
+import "time"
 import "github.com/showwayer/stringutil"
 import "os"
 
 func main() {
+	a := 1
+	b := 3
 	fmt.Println("Starting API Server...")
 	fmt.Println("Creating temp file...")
 	myFile, err := os.Create("tmp.txt")
@@ -16,4 +20,16 @@ func main() {
 	err = myFile.Close()
 	//here we call a function in stringutil
 	fmt.Printf(stringutil.Reverse("!ENOD") + "\n")
+	fmt.Printf("%d + %d = %d", a, b, stringutil.Add(a, b))
+	fmt.Println("The end")
+}
+
+func worker(myChannel chan bool) {
+	fmt.Printf("working on something...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	//return to main thread by setting a boolean
+	myChannel <- true
+
 }
